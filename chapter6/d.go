d0e248f7ea502604bb9e3d4ea8e13b2a3d2f67f3
yang.fan <154055342@qq.com>
2018-04-12 14:36:23 +0800

    update

---
+++ chapter6/d.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var (
+	counter int
+
+	wg sync.WaitGroup
+
+	mutex sync.Mutex
+)
+
+func main(){
+	wg.Add(2)
+
+	go incCounter(1)
+	go incCounter(2)
+
+	wg.Wait()
+	fmt.Printf("counter %d \n", counter)
+}
+
+func incCounter(id int){
+	defer wg.Done()
+	for count := 0; count < 2; count++ {
+		mutex.Lock()
+		{
+			value := counter
+			runtime.Gosched()
+			value++
+			counter = value
+		}
+		mutex.Unlock()
+	}
+
+}
